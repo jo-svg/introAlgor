380ac8012168a5859242976d0e701b810ba081bf
jo-svg <144480253+jo-svg@users.noreply.github.com>
2024-10-23 12:26:27 -0400

    x

--- parking.js
+++ parking.js
@@ -1,20 +1,9 @@
-const n = 5;
-const firstRow = "CC..C";
-const secondRow = ".CC..";
-
-function emptyParking (n, firstRow, secondRow){
-    let output = 0;
-    for (m=0; m<n; m++){
-        firstRow.forEach((i) => {
-            if (i === "C"){
-                output + 1
-            }
-            else if (i === "."){
-                return;
-            }
-        });
+function getSpaces(spaces, yes, today){
+    let count = 0;
+    for( let i = 0; i <spaces; i++){
+        if(yes[i].upper === 'C'&& today[i]==='C'){
+            count++;
+        }
     }
-    console.log(output);
+    return count;
 }
-
-emptyParking(n, firstRow, secondRow);
