23847f3af5c078f7d7c4b5c2d2bd7c56c1305edb
jo-svg <144480253+jo-svg@users.noreply.github.com>
2024-10-25 12:23:42 -0400

    dsadas

--- ihatebinary.js
+++ ihatebinary.js
@@ -11,32 +11,32 @@ const number_thing = [
 
 function convert(number){
     let binary = []
-    number_thing.forEach((number) =>{
+    number_thing.forEach((power) =>{
         if (power.number>number){
-            binary = 0
-            console.log(binary)
-            return;
+            binary += "0"
+            return binary;
         }
         else if (power.number<=number){
             number = number-power.number
-            binary= 1
-            console.log(binary);
+            binary += "1"
+            return binary;
         }
     })
+    console.log(binary)
 }
+convert(24)
 
-
-function revert(binary, space){
+function revert(binary){
     let number = 0
     let binaryString = binary.toString();
-    for (let i = space-1; i >-1; i--){
+    for (let i = 0; i <binaryString.length; i++){
         if (binaryString[i] === '0'){
             continue;
             //dont use return because it will return the number as 0, instead use continue so it continues the function
         }
         else if (binaryString[i] === "1"){
             number_thing.forEach((power)=>{
-                if (space -1 -i === power.power){
+                if (binaryString.length - 1 -i=== power.power){
                     //if space = 4, i = 3 then power = 0
                     //if space = 4, i = 2 then power = 1
                     //if space = 4, i = 1 then power = 2
@@ -49,6 +49,4 @@ function revert(binary, space){
     }
     console.log(number);
 }
-
-revert(1011,4);
 
